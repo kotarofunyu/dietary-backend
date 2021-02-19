c4cda9d796cbc2c5798080e226ceed949f7c7b55
kotarofunyu <k-funyu@sight-visit.com>
2021-02-19 21:40:11 +0900

    [ADD] user has_many tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,7 +6,9 @@
 #  name       :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  has_many :weights, through: :weight_tags
 end
