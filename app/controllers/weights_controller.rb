c3acc8c11977265745c7772813a113f8a7bc27e6
kotarofunyu <k-funyu@sight-visit.com>
2021-02-20 15:43:21 +0900

    [FEAT] API returns weights and its tags

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -4,8 +4,7 @@ class WeightsController < ApplicationController
   before_action :set_weight, only: %i[update destroy]
 
   def index
-    @weights = Weight.where(user_id: @current_user.id).select(:id, :date, :weight, :comment).order(:date)
-    render json: @weights if weights
+    @weights = Weight.includes(:tags).where(user_id: @current_user.id).select(:id, :date, :weight, :comment).order(:date)
   end
 
   def create
