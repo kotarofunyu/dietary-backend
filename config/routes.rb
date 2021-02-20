0e27e731bad5e24731c90c0604dec45b49aef31f
kotarofunyu <k-funyu@sight-visit.com>
2021-02-20 16:27:53 +0900

    jbuilder使い始めてmissing partial出たのでformatを明示的に指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :weights
+  resources :weights, { :format => 'json' }
   resources :users, only: %i[index show create]
   resources :tags, only: %i[index create update destroy]
   post 'signup', controller: :users, action: :create
