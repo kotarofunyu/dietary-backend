030ec8f354d80ea903e2f43407272ffbb0952a6a
kotarofunyu <k-funyu@sight-visit.com>
2021-02-20 15:45:21 +0900

    [REMOVE] select不要

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -4,7 +4,7 @@ class WeightsController < ApplicationController
   before_action :set_weight, only: %i[update destroy]
 
   def index
-    @weights = Weight.includes(:tags).where(user_id: @current_user.id).select(:id, :date, :weight, :comment).order(:date)
+    @weights = Weight.includes(:tags).where(user_id: @current_user.id).order(:date)
   end
 
   def create
